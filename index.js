515d20a6ac8ebf0515690a93ffeb6370fc1fece5
Anton Khlebnikov <toni.farsh@icloud.com>
2024-04-26 22:49:49 +0500

    fix

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const answer = document.getElementById('answer');
 const present = document.getElementById('present');
 const label = document.getElementById('label');
 const imgPresent = document.getElementById('img-present');
-const speech = document.getElementById('speech');
-const audio = new Audio();
-audio.preload = 'auto';
-audio.volume = 0.3;
 
-document.getElementById('bg-sound').volume = 0.2;
-present.addEventListener('click', () => { document.getElementById('bg-sound').play() });
-btn.addEventListener('click', () => { document.getElementById('bg-sound').play() })
+btn.addEventListener('click', () => { document.getElementById('bg-sound').play() });
+
 let soundNo = [
     'no sound/no-bezrazl.mp3',
     'no sound/no-rezko.mp3',
@@ -23,6 +18,8 @@ let soundNo = [
 btn.addEventListener('click', () => {
     if (answer.value == 27) {
         label.style.opacity = '0';
+        var audio = new Audio();
+        audio.preload = 'auto';
         audio.src = 'yes sound/oh-yeah.mp3';
         audio.play();
 
@@ -30,23 +27,20 @@ btn.addEventListener('click', () => {
             present.classList.add('open');
         }, 3000);
         setTimeout(() => {
+            var audio = new Audio();
+            audio.preload = 'auto';
+            audio.src = 'speech.mp3';
+            audio.play();
             imgPresent.style.zIndex = 3;
             imgPresent.style.transform = 'scale(2)';
-            speech.volume = 1;
-            speech.play();
-            setTimeout(() => {
-                imgPresent.addEventListener('click', () => {
-                    var randomIndex = Math.floor(Math.random() * soundNo.length);
-                    audio.src = soundNo[randomIndex];
-                    audio.play();
-                })
-            }, 14000)
         }, 5000);
 
 
     } else {
         answer.value = '';
         var randomIndex = Math.floor(Math.random() * soundNo.length);
+        var audio = new Audio();
+        audio.preload = 'auto';
         audio.src = soundNo[randomIndex];
         audio.play();
     }
