3b3eab0bdebb3bbf0ec42069a57599d79c8b7e39
Anton Khlebnikov <toni.farsh@icloud.com>
2024-04-27 10:09:43 +0500

    fix

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const present = document.getElementById('present');
 const label = document.getElementById('label');
 const imgPresent = document.getElementById('img-present');
 
+btn.addEventListener('click', ()=> {
+    var audioSoun = new Audio();
+    audioSoun.preload = 'auto';
+    audioSoun.src = 'sound.mp3';
+    audioSoun.volume = 0.3;
+    audioSoun.play();
+});
+
+function playNO() {
+    var randomIndex = Math.floor(Math.random() * soundNo.length);
+    var audioNo = new Audio();
+    audioNo.preload = 'auto';
+    audioNo.src = soundNo[randomIndex];
+    audioNo.play();
+};
+function playYes() {
+    var audioYes = new Audio();
+    audioYes.preload = 'auto';
+    audioYes.src = 'yes sound/oh-yeah.mp3';
+    audioYes.play();
+}
 
 let soundNo = [
     'no sound/no-bezrazl.mp3',
@@ -16,27 +37,23 @@ let soundNo = [
 
 btn.addEventListener('click', () => {
     if (answer.value == 27) {
+        playYes();
         label.style.opacity = '0';
         present.classList.add('open');
-        
+
         setTimeout(() => {
             var audio = new Audio();
             audio.preload = 'auto';
             audio.src = 'speech.mp3';
-            audio.canPlayType('audio/mp3; codecs="vorbis"');
             audio.play();
             imgPresent.style.zIndex = 3;
-            imgPresent.style.transform = 'scale(2)';    
+            imgPresent.style.transform = 'scale(2)';
         }, 2000);
 
 
     } else {
         answer.value = '';
-        var randomIndex = Math.floor(Math.random() * soundNo.length);
-        var audio = new Audio();
-        audio.preload = 'auto';
-        audio.src = soundNo[randomIndex];
-        audio.play();
+        playNO();
     }
 });
 
